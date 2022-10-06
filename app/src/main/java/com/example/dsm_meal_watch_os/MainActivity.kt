83dd70b85bb126f424f1c8dbc8a622b0ba060b99
minseo-and <msp05070478@gmail.com>
2022-10-06 19:53:07 +0900

    feat meal_service

--- app/src/main/java/com/example/dsm_meal_watch_os/MainActivity.kt
+++ app/src/main/java/com/example/dsm_meal_watch_os/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.dsm_meal_watch_os
 
 import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import com.example.dsm_meal_watch_os.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -42,5 +46,33 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    fun mealService() {
+
+        val service = ApiClient.getInstance().create(RetrofitService::class.java)
+        val call : Call<Meal> = service.getMeal("3ae2bb166e5d4d3386d552a8dcc39737","json", "G10",
+            "7430310", "$urlDays", "$urlTimes")
+        call.enqueue(object : Callback<Meal> {
+            override fun onResponse(call: Call<Meal>, response: Response<Meal>) {
+                if (response.body()!=null) {
+                    println("성공")
+                    val res = response.body()!!.mealServiceDietInfo[1].row
+                    Log.d("success", response.body().toString())
+                    if (response.isSuccessful) {
+                        for (i in res.indices) {
+                            val obj = res[i]
+                            val str = obj.DDISH_NM.replace("<br/>", "\n")
+                            binding.tet.text = str
+                        }
+                    }
+                }
+            }
+
+            override fun onFailure(call: Call<Meal>, t: Throwable) {
+                println("실패")
+                Log.d("Test", t.toString())
+            }
+        })
     }
 }
