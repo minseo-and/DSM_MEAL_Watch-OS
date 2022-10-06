4f62cf6f25483241522e1fa57a253cc7ef2623fe
minseo-and <msp05070478@gmail.com>
2022-10-06 19:52:33 +0900

    feat add_function

--- app/src/main/java/com/example/dsm_meal_watch_os/MainActivity.kt
+++ app/src/main/java/com/example/dsm_meal_watch_os/MainActivity.kt
@@ -3,6 +3,34 @@ package com.example.dsm_meal_watch_os
 import android.app.Activity
 import android.os.Bundle
 import com.example.dsm_meal_watch_os.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+var urlDays = getUrlDate()
+var urlTimes = getUrlTime()
+
+private fun getUrlDate(): Int {
+    val currentTime: Date = Calendar.getInstance().time
+    val format_yy_mm_dd = "yyyyMMdd"
+    val format2 = SimpleDateFormat(format_yy_mm_dd, Locale.getDefault())
+    val formatDays: String = format2.format(currentTime)
+    return formatDays.toInt()
+}
+
+private fun getUrlTime(): Int {
+
+    var count = 0
+    val calendar = Calendar.getInstance()
+    val time = calendar.time.hours
+
+    if (time in 0..8) {
+        count = 1
+    } else if (time in 8..13) {
+        count = 2
+    } else count = 3
+
+    return count
+}
 
 class MainActivity : Activity() {
 
